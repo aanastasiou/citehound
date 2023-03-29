7d9b51d188554fc872ab37cda1f10950dbd3f87d
Athanasios Anastasiou <athanastasiou@gmail.com>
2023-03-29 10:00:20 +0000

    Revised the standard queries

--- provisional_queries.md
+++ provisional_queries.md
@@ -7,13 +7,13 @@ This is a list of essential queries that should be provided along with the defau
 ### Articles Per Year
 
 ```
-MATCH (a:Article) RETURN COUNT(a) AS n_articles, date(a.pub_date).year AS year ORDER BY year DESC
+MATCH (a:Article) RETURN date(a.pub_date).year AS year, COUNT(a) AS n_articles ORDER BY year DESC
 ```
 
 ### Articles of a specific year
 
 ```
-MATCH (a:Article) WHERE date(a.pub_date).year={YEAR} RETURN a.article_id, a.doi, a.title, date(a.pub_date) as pub_date ORDER BY pub_date DESC
+MATCH (a:Article) WHERE date(a.pub_date).year={YEAR} RETURN a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC
 ```
 
 Where:
@@ -26,13 +26,13 @@ Where:
 ### Number of articles per Author
 
 ```
-MATCH (a:Author)<-[r:AUTHORED_BY]-() RETURN id(a) as author_id, a.full_name AS author_name, COUNT(r) AS n_articles_authored ORDER BY n_articles_authored DESC
+MATCH (a:Author)<-[r:AUTHORED_BY]-() RETURN id(a) as author_id AS author_id, a.full_name AS author_name, COUNT(r) AS n_articles_authored ORDER BY n_articles_authored DESC
 ```
 
 ### Articles of a given author
 
 ```
-MATCH (a:Article)-[:AUTHORED_BY]->(u:Author) WHERE id(u)={AUTHOR_ID} RETURN a.article_id, a.doi, a.title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC
+MATCH (a:Article)-[:AUTHORED_BY]->(u:Author) WHERE id(u)={AUTHOR_ID} RETURN a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC
 ```
 
 Where:
@@ -42,7 +42,7 @@ Where:
 ### Distinct affiliations per author
 
 ```
-MATCH (a:Author)-[r:AFFILIATED_WITH]->(b:Affiliation) return id(a) as author_id, a.full_name as full_name, count(b) as affiliation_count order by cnt desc
+MATCH (a:Author)-[r:AFFILIATED_WITH]->(b:Affiliation) RETURN id(a) AS author_id, a.full_name AS full_name, count(b) AS affiliation_count ORDER BY cnt DESC
 ```
 
 
@@ -51,19 +51,19 @@ MATCH (a:Author)-[r:AFFILIATED_WITH]->(b:Affiliation) return id(a) as author_id,
 ### Number of articles per institute
 
 ```
-MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(:Author)<-[:AUTHORED_BY]-(a:Article) return i.grid as institute_id, i.name as institute_name, count(a) as n_articles_produced ORDER BY n_articles_produced DESC
+MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(:Author)<-[:AUTHORED_BY]-(a:Article) RETURN i.grid AS institute_id, i.name AS institute_name, COUNT(a) AS n_articles_produced ORDER BY n_articles_produced DESC
 ```
 
 ### Number of authors per institute
 
 ```
-MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(u:Author) return i.grid as institute_id, i.name as institute_name, count(u) as n_affiliated_authors ORDER BY n_affiliated_authors DESC
+MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(u:Author) RETURN i.grid AS institute_id, i.name AS institute_name, count(u) AS n_affiliated_authors ORDER BY n_affiliated_authors DESC
 ```
 
 ### Articles associated with a given institute
 
 ```
-MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(:Author)<-[:AUTHORED_BY]-(a:Article) WHERE i.grid="{INSTITITE_GRID}" RETURN distinct a.article_id, a.doi, a.title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC
+MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(:Author)<-[:AUTHORED_BY]-(a:Article) WHERE i.grid="{INSTITITE_GRID}" RETURN DISTINCT a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC
 ```
 
 Where:
@@ -116,12 +116,11 @@ MATCH (a:Article)
 			         MATCH (:Affiliation)-[r:ASSOCIATED_WITH{rel_label:"FROM_COUNTRY"}]->()
 				       WITH n_articles, n_authors, n_affiliations, COUNT(r) AS n_affiliations_asc_to_country
 				            MATCH (:Affiliation)-[r:ASSOCIATED_WITH{rel_label:"FROM_INSTITUTE"}]->()
-					          return n_articles, n_authors, n_affiliations, n_affiliations_asc_to_country, COUNT(r) as n_affiliations_asc_to_institute
+					          RETURN n_articles, n_authors, n_affiliations, n_affiliations_asc_to_country, COUNT(r) as n_affiliations_asc_to_institute
 ```
 
 ### Affiliations to institute per year
 ```
-MATCH (a:Article)-[:AUTHORED_BY]->(:Author)-[:AFFILIATED_WITH]->(:Affiliation)-[r:ASSOCIATED_WITH{rel_label:"FROM_INSTITUTE"}]->()
-					          return COUNT(r) as n_affiliations_asc_to_institute, date(a.pub_date).year as year order by year desc
+MATCH (a:Article)-[:AUTHORED_BY]->(:Author)-[:AFFILIATED_WITH]->(:Affiliation)-[r:ASSOCIATED_WITH{rel_label:"FROM_INSTITUTE"}]->() RETURN COUNT(r) AS n_affiliations_asc_to_institute, date(a.pub_date).year AS year ORDER BY year DESC
 ```
 
--- scripts/citehound_admin.py
+++ scripts/citehound_admin.py
@@ -473,22 +473,42 @@ def init():
     """
     # Add a set of pre-defined queries
     queries=[("N_ARTICLES_PER_YEAR",
-              "MATCH (a:Article) RETURN COUNT(a) AS n_articles, date(a.pub_date).year AS year ORDER BY year DESC",
-              "Number of articles per year"),
+              "MATCH (a:Article) RETURN date(a.pub_date).year AS year, COUNT(a) AS n_articles ORDER BY year DESC",
+              "Number of articles per year across the whole dataset"),
              ("ARTICLES_OF_YEAR",
-              "MATCH (a:Article) WHERE date(a.pub_date).year=$year RETURN a.article_id, a.doi, a.title, date(a.pub_date) as pub_date ORDER BY pub_date DESC",
-              "A list of articles uploaded within a specific year. Expects parameter 'year'"),
+              "MATCH (a:Article) WHERE date(a.pub_date).year=$year RETURN a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC",
+              "List of articles added to the dataset on the specific year. Must specify the 'year' parameter"),
              ("N_ARTICLES_PER_AUTHOR",
-              "MATCH (a:Author)<-[r:AUTHORED_BY]-() RETURN id(a) as author_id, a.full_name AS author_name, COUNT(r) AS n_articles_authored ORDER BY n_articles_authored DESC",
+              "MATCH (a:Author)<-[r:AUTHORED_BY]-() RETURN id(a) AS author_id, a.full_name AS author_name, COUNT(r) AS n_articles_authored ORDER BY n_articles_authored DESC",
               "Number of articles per author"),
              ("ARTICLES_OF_AUTHOR",
-              "MATCH (a:Article)-[:AUTHORED_BY]->(u:Author) WHERE id(u)=$author_id RETURN a.article_id, a.doi, a.title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC",
-              "A list of articles authored by a specific author. Expects parameter 'author_id', see N_ARTICLES_PER_AUTHOR")  
-            ]
+              "MATCH (a:Article)-[:AUTHORED_BY]->(u:Author) WHERE id(u)=$author_id RETURN a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC",
+              "List of articles attributed to a specific author. Must specify 'author_id'. See query N_ARTICLES_PER_AUTHOR query to recover a given 'author_id'"),
+             ("N_AFFILIATIONS_PER_AUTHOR",
+              "MATCH (a:Author)-[r:AFFILIATED_WITH]->(b:Affiliation) RETURN id(a) AS author_id, a.full_name AS full_name, count(b) AS affiliation_count ORDER BY cnt DESC",
+              "Number of affiliations per author"),
+             ("N_ARTICLES_PER_INSTITUTE",
+              "MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(:Author)<-[:AUTHORED_BY]-(a:Article) RETURN i.grid AS institute_id, i.name AS institute_name, COUNT(a) AS n_articles_produced ORDER BY n_articles_produced DESC",
+              "Number of articles per institute. Articles must have been linked, see 'db link'"),
+             ("N_AUTHORS_PER_INSTITUTE",
+              "MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(u:Author) RETURN i.grid AS institute_id, i.name AS institute_name, count(u) AS n_affiliated_authors ORDER BY n_affiliated_authors DESC",
+              "Number of authors affiliated with their respective institutes"),
+             ("ARTICLES_OF_INSTITUTE",
+              "MATCH (i:Institute)<-[:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]-(:Affiliation)<-[:AFFILIATED_WITH]-(:Author)<-[:AUTHORED_BY]-(a:Article) WHERE i.grid=$institute_grid RETURN DISTINCT a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC",
+              "Articles per institute. The linking operation must have run first, see 'db link'. Expects parameter 'institute_grid', see N_ARTICLES_PER_INSTITUTE on how to recover one"),
+             ("N_ARTICLES_PER_COUNTRY",
+              "MATCH (a:Article)-[:AUTHORED_BY]->(u:Author)-[:AFFILIATED_WITH]->(f:Affiliation)-[:ASSOCIATED_WITH]-(c:Country) RETURN c.code AS country_code, c.name AS country_name, COUNT(a) AS articles_produced ORDER BY articles_produced DESC",
+              "Number of articles associated with a specific country"),
+             ("ARTICLES_OF_COUNTRY",
+              "MATCH (a:Article)-[:AUTHORED_BY]->(u:Author)-[:AFFILIATED_WITH]->(f:Affiliation)-[:ASSOCIATED_WITH]-(c:Country) WHERE c.code=$country_code RETURN  a.article_id AS article_id, a.doi AS doi, a.title AS title, date(a.pub_date) AS pub_date ORDER BY pub_date DESC",
+              "Articles associated with a specific country. Articles must have been linked, see 'db link'. Expects parameter 'country_code')"),
+              ("LINK_DIAGNOSTIC",
+               "MATCH (a:Article) WITH COUNT(a) AS n_articles MATCH (u:Author) WITH n_articles, COUNT(u) AS n_authors MATCH (f:Affiliation) WITH n_articles, n_authors, COUNT(f) AS n_affiliations MATCH (:Affiliation)-[r:ASSOCIATED_WITH{rel_label:'FROM_COUNTRY'}]->() WITH n_articles, n_authors, n_affiliations, COUNT(r) AS n_affiliations_asc_to_country MATCH (:Affiliation)-[r:ASSOCIATED_WITH{rel_label:'FROM_INSTITUTE'}]->() RETURN n_articles, n_authors, n_affiliations, n_affiliations_asc_to_country, COUNT(r) as n_affiliations_asc_to_institute",
+               "Returns total numbers of papers, authors, affiliations, affiliations linked to an institute, affiliations linked to a country.")]
 
     # Check if STD_QUERIES is already defined
     try:
-        the_map = neoads.AbstractMap.nodes(name="STD_QUERIES")
+        the_map = neoads.AbstractMap.nodes.get(name="STD_QUERIES")
     except neoads.AbstractMap.DoesNotExist as e:
         # If not then initialise it
         the_map = neoads.AbstractMap(name="STD_QUERIES").save()
